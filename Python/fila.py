3ea3210eb5b7ac0fed8e0d9c2118c252efb5bc64
marcelinaquimbungo5 <marcelinaquimbungo5@gmail.com>
2026-03-25 14:08:12 +0100

    Update fila.py

--- Python/fila.py
+++ Python/fila.py
@@ -1,1 +1,39 @@
 
+class Fila:
+    def __init__(self):
+        # Inicializa a fila como lista vazia
+        self.fila = []
+
+    # Inserção -- enqueue
+    def enqueue(self, item):
+        self.fila.append(item)
+
+    # Remoção -- dequeue
+    def dequeue(self):
+        if not self.is_empty():
+            return self.fila.pop(0)
+        return None
+
+    # Consulta do 1º elemento -- front
+    def front(self):
+        if not self.is_empty():
+            return self.fila[0]
+        return None
+
+    # Ver se a fila está vazia
+    def is_empty(self):
+        return len(self.fila) == 0
+
+
+if __name__ == "__main__":
+    fila = Fila()
+    fila.enqueue(1)
+    fila.enqueue(2)
+    fila.enqueue(3)
+    fila.enqueue(4)
+    fila.enqueue(5)
+    fila.enqueue(6)
+
+    print("Front:", fila.front())
+    print("1º a sair:", fila.dequeue())
+    print("Está vazia?:", fila.is_empty())
