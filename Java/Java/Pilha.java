1816c03fe5067eded49120a56e7fb062097c41d3
marcelinaquimbungo5 <marcelinaquimbungo5@gmail.com>
2026-03-25 13:53:19 +0100

    Add stack implementation in Pilha class
    Implemented stack operations including push, pop, peek, and isEmpty methods.

--- Java/Java/Pilha.java
+++ Java/Java/Pilha.java
@@ -1,1 +1,40 @@
+import java.util.ArrayList;
+public class Pilha {
+    private ArrayList<Integer> pilha = new ArrayList<>();
 
+    //Inserçáo -- push
+    public void push(int item){
+        pilha.add(item);
+    }
+    //Remoçáo -- pop
+    public Integer pop(){
+        if (!pilha.isEmpty()){
+            return pilha.remove(pilha.size()-1);
+        }
+        return null;
+    }
+    //Consulta do Topo -- peek
+    public Integer peek(){
+        if (!pilha.isEmpty()){
+            return pilha.get(pilha.size()-1);
+        }
+        return null;
+    }
+    //Ver se está Vazia
+    public boolean isEmpty(){
+        return pilha.isEmpty();
+    }
+    //class main
+    public static void main(String[] args){
+        Pilha pilha = new Pilha();
+        pilha.push(1);
+        pilha.push(2);
+        pilha.push(3);
+        pilha.push(4);
+        pilha.push(5);
+
+        System.out.println("Elemento no Topo: "+pilha.peek());
+        System.out.println("Primeiro a Sair: "+pilha.pop());
+        System.out.println("Está Vazia?: "+pilha.isEmpty());
+    }
+}
